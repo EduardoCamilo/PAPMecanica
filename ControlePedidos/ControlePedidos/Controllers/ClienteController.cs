2d22b894a191210c205cfd6823b01169a74f1b3e
ALYSONSIQUEIRAINF <alysonsiqueira.inf@gmail.com.br>
2017-07-03 20:55:27 -0300

    Classe cliente funcionando

--- ControlePedidos/ControlePedidos/Controllers/ClienteController.cs
+++ ControlePedidos/ControlePedidos/Controllers/ClienteController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace ControlePedidos.Controllers
 {
     public class ClienteController : Controller
@@ -18,7 +19,7 @@ namespace ControlePedidos.Controllers
         public ActionResult Index()
         {
             var clientes = db.Clientes.ToList();
-            return View();
+            return View(clientes);
         }
         //GET
         public ActionResult Create()
